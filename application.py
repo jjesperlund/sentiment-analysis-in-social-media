145791e2cff48316c0fe4dc4b130dcb4dc880753
David Tran <davidtran19@yahoo.com>
2018-09-11 11:00:44 +0200

    Testing python flask backend. Saved naivebayes model

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+import requests
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,6 +7,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/category=<category>")
+def category_route(category):
+    category_variable = category
+    get = requests.get('http://localhost:5100/api/youtube-videos?category='+category_variable+'&count=3')
+    print(get.text)
+    return 'GG'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
