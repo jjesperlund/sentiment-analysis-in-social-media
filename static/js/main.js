71dcf32648666314a41b9a24d8d566d9493fbfab
David Tran <davidtran19@yahoo.com>
2018-09-18 10:49:11 +0200

    HTTP GET request json data

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,6 @@
 var category = window.location.search.split('=')[1]
+console.log(category)
+$.get( "/category=" + category, function(data){
+    var sentiment_data = $.parseJSON(data);
+    console.log(sentiment_data)
+})
