e996d8da86d47ab50408d359d8a9a8f90462d9ef
Jesper Lund <jeslu246@student.liu.se>
2018-09-21 15:00:02 +0200

    Add word cloud, table

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,18 @@
-
 window.onload = () => {
+
+  let tab = document.getElementById('Overview');
+  tab.style.display = "block";
+  tab.className += " active";
+
   var category = window.location.search.split('=')[1]
   console.log('Fetching data about: ', category); 
 
+  /*
   $.get( "/category=" + category, function(data){
       var sentiment_data = $.parseJSON(data);
       console.log(sentiment_data)
   })
+  */
 
   var table = document.getElementById("table");
   createTable(table);
--- static/js/table.js
+++ static/js/table.js
@@ -8,12 +8,16 @@ createTable = (table) => {
   //for (let i in dataArray )
   for (let row = 0; row < 35; row++) {
 
-      var newRow = table.insertRow(-1);
-      // Loop number of cells in each row
-      for (let col = 0; col <= 3; col++) {
-          let cell = newRow.insertCell(col);
-          cell.innerHTML = 'some data prop';
-      }
-
+    var newRow = table.insertRow(-1);
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+    let cell3 = newRow.insertCell(2);
+    let cell4 = newRow.insertCell(3);
+    
+    cell1.innerHTML = 
+    '<img src="../static/assets/' + 'twitter' + '_logo.jpg" style="width:55px;height:30px;" alt="social_media_logo"/>';
+    cell2.innerHTML = '@' + '--username--';
+    cell3.innerHTML = '--comment--';
+    cell4.innerHTML = '--timestamp--';
   }
 }
