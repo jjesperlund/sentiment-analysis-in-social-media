305780af4b74b5170eb1e44fa78015d9150c8850
David Tran <davidtran19@yahoo.com>
2018-09-28 12:50:49 +0200

    Fixed json array

--- application.py
+++ application.py
@@ -13,6 +13,8 @@ classifier_f.close()
 def retrieveYoutube(category):
     get_youtube = requests.get('http://127.0.0.1:5100/api/youtube-videos?category='+category+'&count=10')
     response_json = get_youtube.json()
+    #print(response_json)
+    test = True
     json_vec = []
     for i in response_json:
         comments_response = requests.get('http://127.0.0.1:5100/api/youtube-comments?videoID='+i['videoId']+'&count=10')
@@ -21,17 +23,24 @@ def retrieveYoutube(category):
             comment = processVec(c['comment'].lower())
             sentiment = classifier.classify(word_feature_vec(comment)) #pos eller neg
             c['sentiment'] = sentiment
-    json_vec.append(json_data)
+            c['videoTitle'] = i['title']
+            json_vec.append(c)
 
     return json_vec
 
 def retrieveTwitter(category):
-    get_twitter = requests.get('http://127.0.0.1:5100/api/tweets?category=' +category+'&count=100')
+    get_twitter = requests.get('http://127.0.0.1:5100/api/tweets?category=' +category+'&count=10')
     response_json = get_twitter.json()
+    #print(response_json)
     json_vec = []
     for json_obj in response_json:
         tweet = clean_tweet(json_obj['tweetText'])
         sentiment = classifier.classify(word_feature_vec(tweet))
+        # prob_dist = classifier.prob_classify(word_feature_vec(tweet))
+        # print(json_obj['tweetText'])
+        # for label in prob_dist.samples():
+        #     print("%s: %f" % (label, prob_dist.prob(label)))
+                
         json_obj['sentiment'] = sentiment
         json_vec.append(json_obj)
     
@@ -50,11 +59,17 @@ def index():
 @app.route("/category=<category>")
 def category_route(category):
     category_variable = category
-    youtube_vec = retrieveYoutube(category_variable)
+    #youtube_vec = retrieveYoutube(category_variable)
     twitter_vec = retrieveTwitter(category_variable)
 
-    json_vec = youtube_vec + twitter_vec
+    json_vec = twitter_vec #+youtube_vec
     json_vec = json.dumps(json_vec)
+    #with open('youtubeTwitter.json', 'w') as outfile:
+        #json_vec = json.dumps(json_vec)
+        #json_dat = json.loads(json_vec)
+        #print(json_dat)
+        #print(json_dat)
+        #json.dump(json_dat, outfile)
     return json_vec
     
 
